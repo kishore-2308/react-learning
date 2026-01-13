3ee53d33c0c6640d52327d7ae113fdefb7846f5b
Mani Kishore Amudam <manikishore_amudam@epam.com>
2026-01-13 17:42:55 +0530

    module-1 3 tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
 import React from "react";
-import Counter from "./Counter"
+import { useState } from "react";
+import GenreSelect from "./GenreSelect";
+// import Counter from "./Counter"
+// import SearchForm from "./SearchForm.jsx";
 
 
 function App() {
- return React.createElement(
-    Counter,
-    { initialValue: 0 }
+//  return React.createElement(
+//     Counter,
+//     { initialValue: 0 }
+//   );
+
+// return (
+//   <SearchForm
+//   initialQuery="react"
+//   onSearch={(value) => {
+//     console.log("Searching for:", value);
+//   }}
+// />
+// )
+const genres = ["All", "Action", "Comedy", "Drama"];
+  const [selectedGenre, setSelectedGenre] = useState("All");
+
+  const handleGenreSelect = (genre) => {
+    setSelectedGenre(genre);
+  };
+
+  return (
+    <div>
+      <GenreSelect
+        genres={genres}
+        selectedGenre={selectedGenre}
+        onSelect={handleGenreSelect}
+      />
+
+      <p>Selected genre: {selectedGenre}</p>
+    </div>
   );
 }
 export default App;
